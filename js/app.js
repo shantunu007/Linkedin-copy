8d599a9e6cabdf75fd6036cc78a10a159003e1dc
Shantunu Chakma <138578549+shantunu007@users.noreply.github.com>
2023-08-04 13:18:29 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+    sideActivity.classList.toggle(".open-activity");
+}
